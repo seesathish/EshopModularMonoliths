27994fbf1fa48f16a17e831a0c403ba73fb2c6ca
Sathish <seesathish@gmail.com>
2024-09-23 13:39:00 +0530

    Modular Monolithic EShop base code with API, Shared, Modules (Catalog, Basket and Ordering) implemented.

---
+++ src/Shared/Shared/DDD/IAggregate.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Security.Principal;
+
+namespace Shared.DDD;
+
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+}
+
+public interface IAggregate : IEntity
+{
+    IReadOnlyList<IDomainEvent> DomainEvents { get; }
+    IDomainEvent[] ClearDomainEvents();
+}
